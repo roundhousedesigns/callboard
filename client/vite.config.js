ce757b917b5a3426e6f536604f340c1d224effb0
Nick Gaswirth <nick@roundhouse-designs.com>
2026-02-22 13:36:11 -0500

    Add allowed hosts for development server
    - Configured the Vite development server to allow requests from "callboard.roundhouse-designs.com".
    - This change enhances the local development experience by enabling access from specified hosts.

--- client/vite.config.js
+++ client/vite.config.js
@@ -36,6 +36,7 @@ export default defineConfig({
     },
     server: {
         port: 5173,
+        allowedHosts: ["callboard.roundhouse-designs.com"],
         proxy: {
             "/api": {
                 target: "http://localhost:3001",
